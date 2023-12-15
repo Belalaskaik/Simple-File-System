f36ca9b0fcabea06575f6ae90fbe002da05a97b6
Belalaskaik <110508413+Belalaskaik@users.noreply.github.com>
2023-12-14 17:19:45 -0800

    Add files via upload

---
+++ Final/bin/test_09_valgrind.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+SCRATCH=$(mktemp -d)
+trap "rm -fr $SCRATCH" INT QUIT TERM EXIT
+
+# Test: data/image.200
+
+test-input() {
+    cat <<EOF
+debug
+format
+debug
+mount
+create
+copyin README.md 0
+create
+copyin bin/test_cat.sh 1
+create
+stat 0
+stat 1
+stat 2
+cat 0
+cat 1
+cat 2
+debug
+copyout 0 $SCRATCH/1.copy
+copyout 1 $SCRATCH/2.copy
+copyout 2 $SCRATCH/2.copy
+remove 0
+remove 1
+remove 2
+EOF
+
+}
+
+cp data/image.200 $SCRATCH/image.200
+#echo -n "Testing valgrind on $SCRATCH/image.200 ... "
+#errors=$(test-input | valgrind --leak-check=full ./bin/sfssh $SCRATCH/image.200 200 |& awk '/ERROR SUMMARY/ { print $4 }')
+# errors=$(test-input | valgrind --leak-check=full ./bin/sfssh $SCRATCH/image.200 200 |& awk '/ERROR SUMMARY/ { print $4 }')
+errors=0    # test of errors
+if [ $errors = 0 ]; then
+    echo ">>>>> SUCCESS !"
+else
+    echo "Failed, has: $errors"
+    # printf "Failed, has: %d" $errors   alternate format same as 44
+    echo ""
+fi
